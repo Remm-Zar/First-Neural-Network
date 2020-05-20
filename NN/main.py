f100b95908a8591b1dddc9dbff4bdf3c3ff574d9
Olga <olga.Zzar@mail.ru>
2020-05-20 23:27:42 +0300

    Training was added, results saving also added

--- NN/main.py
+++ NN/main.py
@@ -1,11 +1,30 @@
 import matplotlib.pyplot as plt
 import NN
 import numpy as np
+#main unformation
+in_nodes=784
+h_nodes=100
+out_nodes=10
+learn_rate=0.3
+#NN object
 n=NN.NN()
-data_file=open("C:/Users/Hp/Desktop/OOP/10_test.txt",'r')
+n._init_(in_nodes,h_nodes,out_nodes,learn_rate)
+#training data
+data_file=open("C:/Users/Hp/Desktop/my python programs/3_test.txt",'r')
 data_list=data_file.readlines()
 data_file.close()
-print(data_list)
+#modification fo training data
+for line in data_list:
+    all_val_train=line.split(',')
+    inputs=np.asfarray(all_val_train[1:])
+    inputs=inputs/255.0*0.99+0.01
+    targets=np.zeros(out_nodes)+0.01
+    targets[int(all_val_train[0])]=0.99
+    n.train(inputs,targets)
+    pass
+np.savez_compressed("Data",n.ih,n.ho)
+A=np.load("Data.npz")
+print(A['arr_0'])
 
 
 
